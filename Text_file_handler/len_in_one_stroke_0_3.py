70f479147c921d193cd399b2dceacb2f327a2204
PetroffSky <ds9657@yandex.ru>
2023-06-06 14:15:26 +0300

    Обработка текстовых данных по шаблону и подсчёт символов в строке.

---
+++ Text_file_handler/len_in_one_stroke_0_3.py
@@ -0,0 +1,39 @@
+
+
+def script_120():
+    with open("input_stroke.txt", "r", encoding="utf-8") as infile, \
+        open("output_stroke.txt", "w", encoding="utf-8") as outfile:
+
+        for index, stroke in enumerate(infile.read().split("\n"), 1):
+            if index > 8:
+                print(f"Количество строк достигло больше 8, а именно {index}")
+            elif len(stroke) > 120:
+                print(f"Длина {index}-й строки {len(stroke)} символов")
+                continue
+            elif len(stroke) > 0:
+                print(f"Длина строки {len(stroke)} символов")
+                new_stroke = f"— {stroke}  \n"
+                outfile.write(new_stroke)
+
+                
+
+def script_180():
+    with open("input_stroke.txt", "r", encoding="utf-8") as infile, \
+        open("output_stroke.txt", "w", encoding="utf-8") as outfile:
+        total_len = 0
+
+        for index, stroke in enumerate(infile.read().split("\n"), 1):
+            total_len += len(stroke)
+            if index > 8:
+                print(f"Количество строк: {index}")
+            elif len(stroke) > 180:
+                print(f"Длина {index}-й строки {len(stroke)} символов")
+                continue
+            elif len(stroke) > 0:
+                print(f"Длина строки {len(stroke)} символов")
+                new_stroke = f"— {stroke}  \n"
+                outfile.write(new_stroke)
+
+        print(f"Общая длина текста составляет: {len(stroke)} символов")
+
+
---
+++ Text_file_handler/main.py
@@ -0,0 +1,16 @@
+import len_in_one_stroke_0_3
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    with open(".settings", "r", encoding="utf-8") as settings:
+
+        for stroke in settings.read().split("\n"):
+            if stroke[stroke.find(" ") + 1:] == "1":  # 1-й режим запускает скрипт на 120 символов
+                len_in_one_stroke_0_3.script_120()
+            elif stroke[stroke.find(" ") + 1:] == "2":  # 1-й режим запускает скрипт на 180 символов
+                len_in_one_stroke_0_3.script_180()
