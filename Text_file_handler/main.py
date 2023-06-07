917a1ad504f04d3ae0229d21f1f9d27c8c53673f
PetroffSky <ds9657@yandex.ru>
2023-06-07 19:59:39 +0300

    Оптимизирован алгоритм работы программы

--- Text_file_handler/main.py
+++ Text_file_handler/main.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
 
         for stroke in settings.read().split("\n"):
             if stroke[stroke.find(" ") + 1:] == "1":  # 1-й режим запускает скрипт на 120 символов
-                len_in_one_stroke_0_3.script_120()
+                len_in_one_stroke_0_3.file_readwrite(1)
             elif stroke[stroke.find(" ") + 1:] == "2":  # 1-й режим запускает скрипт на 180 символов
-                len_in_one_stroke_0_3.script_180()
+                len_in_one_stroke_0_3.file_readwrite(2)
 
-
+    print()
     with open("output_stroke.txt", "r", encoding="utf-8") as file:
         print(file.read())
